09a9869936a64a8f6bc6cf7a7cd4c233e3aaa3b7
CodeGhost <bellinuolaji@gmail.com>
2026-06-26 07:35:28 +0100

    db_conn update

--- DB/db_conn.php
+++ DB/db_conn.php
@@ -1,14 +1,13 @@
 <?php
-$host = 'aws-0-eu-west-1.pooler.supabase.com'; 
-$port = '6543';
-$db_name = 'postgres';
-$db_user = 'postgres.mtkecdfrvpbphdjciyht'; 
-$db_pass = '8tDSITrGVNWHEnop';
+$host = getenv('DB_HOST')?: 'aws-0-eu-west-1.pooler.supabase.com';
+$port = getenv('DB_PORT')?: '6543';
+$db_name = getenv('DB_NAME')?: 'postgres';
+$db_user = getenv('DB_USER')?: '';
+$db_pass = getenv('DB_PASS')?: '';
 
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db_name", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully!";
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
